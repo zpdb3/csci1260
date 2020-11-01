1be802297990b0d2fc43da66176e34d821911446
zpdb3 <73575970+zpdb3@users.noreply.github.com>
2020-11-01 13:35:03 -0500

    Add files via upload

---
+++ LectureDriver.java
@@ -0,0 +1,25 @@
+public class LectureDriver
+{
+  public static void main(String[] args)
+  {
+    Sword sword = new Sword();
+//    sword.setCondition(10); 
+    LongBow bow = new LongBow();
+//    bow.attack(hitChance 5);
+//    bow.attack();
+//    ElvishLeatherArmor armor = new ElvishLeatherArmor();
+    Armor ElvishLeatherArmor = new Armor(); 
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+}
