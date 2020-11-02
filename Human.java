961bf3442025bda1f9a2e01dd565769fe7af2536
PIPBoy0311 <73849305+PIPBoy0311@users.noreply.github.com>
2020-11-02 13:45:55 -0500

    Create Human.java

---
+++ Human.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class Human extends Person {
+
+    public Human() {
+        super("Villager");
+        ArrayList<String> villagerWordSalad = new ArrayList<>();
+        villagerWordSalad.add("I used to be an adventurer like you, then I took an arrow to the knee.");
+        villagerWordSalad.add("Help!  Help!  I'm being repressed! Come see the violence inherent in the system!");
+        villagerWordSalad.add("Oh, but you cant't expect to wield supreme executive power just because some watery " +
+                              "tart threw a sword at you!");
+        super.setThingsToSay(villagerWordSalad);
+    }
+
+    public Human(String name) {
+        super(name);
+        ArrayList<String> villagerWordSalad = new ArrayList<>();
+        villagerWordSalad.add("I used to be an adventurer like you, then I took an arrow to the knee.");
+        villagerWordSalad.add("Help!  Help!  I'm being repressed! Come see the violence inherent in the system!");
+        villagerWordSalad.add("Oh, but you cant't expect to wield supreme executive power just because some watery " +
+                "tart threw a sword at you!");
+        super.setThingsToSay(villagerWordSalad);
+    }
+}
