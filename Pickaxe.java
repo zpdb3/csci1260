190affc9bd350bb3e0bd03884ff5bce5e9460027
zpdb3 <73575970+zpdb3@users.noreply.github.com>
2020-12-05 07:16:01 -0500

    Add files via upload

---
+++ Pickaxe.java
@@ -0,0 +1,22 @@
+// ******
+// Filename:   Pickaxe
+// Team:       Big Bois
+// Team Members: Austen Boda, Duncan Hayes, Eric Caton,
+//                               Jason Joyce, and Paul Brummitt
+// ******
+// The implementation of Pickaxe will be lead by Paul Brummitt
+
+public class Pickaxe  {
+
+    public Pickaxe() {
+//        super();
+//        setName("Pickaxe");
+//        setNormalDamage(8);
+//        setPrice(125);
+//        setOptimalTool(OptimalTool.Pickaxe);
+    }
+
+//    public Pickaxe(int dmg, int ice, int fire, WeaponHandedness wp, int price, double durability, double range, int maxAmmo) {
+//        super("Throwing Pickaxe", dmg, ice, fire, wp, price, durability, range, maxAmmo);
+//    }
+}
